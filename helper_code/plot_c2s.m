0bf72223b29ae1a8c1ca7e2a05e1a89f45ca34ea
Aashay Patel <aashaypatel2021@gmail.com>
2022-03-23 16:48:28 -0400

    Corrected misorganized code that printed statistics unnecessarily

--- helper_code/plot_c2s.m
+++ helper_code/plot_c2s.m
@@ -45,73 +45,6 @@ for k = 1:lenm % metric
         end
     end
     
-    % Collect the data for correlations and information for each monkey and
-    % condition
-    kaki_nacl_corr = met(met(:,1)==0 & met(:,2)==0, 3:4);
-    kaki_5HT_corr = met(met(:,1)==0 & met(:,2)==1, 3:4);
-    kaki_nacl_info = met(met(:,1)==0 & met(:,2)==0, 5:6);
-    kaki_5HT_info = met(met(:,1)==0 & met(:,2)==1, 5:6);
-    
-    mango_nacl_corr = met(met(:,1)==1 & met(:,2)==0, 3:4);
-    mango_5HT_corr = met(met(:,1)==1 & met(:,2)==1, 3:4);
-    mango_nacl_info = met(met(:,1)==1 & met(:,2)==0, 5:6);
-    mango_5HT_info = met(met(:,1)==1 & met(:,2)==1, 5:6);
-    
-    
-    % Perform statistical tests for Correlation data for each monkey (and
-    % together)
-    stats_kaki_corr = pair_tests(kaki_nacl_corr, kaki_5HT_corr);
-    disp(['Kaki (NaCl; n=' num2str(size(kaki_nacl_corr, 1)) ') ------------------------'])
-    disp(stats_kaki_corr.pair(1).table)
-    disp(['Kaki (5HT; n=' num2str(size(kaki_5HT_corr, 1)) ') ------------------------'])
-    disp(stats_kaki_corr.pair(2).table)
-    disp(['Kaki (NaCl vs 5HT; n=' num2str(size(kaki_5HT_corr, 1)+size(kaki_nacl_corr, 1)) ') ---------'])
-    disp(stats_kaki_corr.table)
-    
-    stats_mango_corr = pair_tests(mango_nacl_corr, mango_5HT_corr);
-    disp(['Mango (NaCl; n=' num2str(size(mango_nacl_corr, 1)) ') ------------------------'])
-    disp(stats_mango_corr.pair(1).table)
-    disp(['Mango (5HT; n=' num2str(size(mango_5HT_corr, 1)) ') ------------------------'])
-    disp(stats_mango_corr.pair(2).table)
-    disp(['Mango (NaCl vs 5HT; n=' num2str(size(mango_5HT_corr, 1)+size(mango_nacl_corr, 1)) ') ---------'])
-    disp(stats_mango_corr.table)
-    
-    stats_all_corr = pair_tests([kaki_nacl_corr; mango_nacl_corr], [kaki_5HT_corr; mango_5HT_corr]);
-    disp(['Both (NaCl; n=' num2str(size(mango_nacl_corr, 1)+size(kaki_nacl_corr, 1)) ') ------------------------'])
-    disp(stats_all_corr.pair(1).table)
-    disp(['Both (5HT; n=' num2str(size(mango_5HT_corr, 1)+size(kaki_5HT_corr, 1)) ') ------------------------'])
-    disp(stats_all_corr.pair(2).table)
-    disp(['Both (NaCl vs 5HT; n=' num2str(size(mango_nacl_corr, 1)+size(mango_5HT_corr, 1)+size(kaki_nacl_corr, 1)+size(kaki_5HT_corr, 1)) ') ---------'])
-    disp(stats_all_corr.table)
-    
-    
-    % Perform statistical tests for Information data for each monkey (and
-    % together)
-    stats_kaki_info = pair_tests(kaki_nacl_info, kaki_5HT_info);
-    disp(['Kaki (NaCl; n=' num2str(size(kaki_nacl_info, 1)) ') ------------------------'])
-    disp(stats_kaki_info.pair(1).table)
-    disp(['Kaki (5HT; n=' num2str(size(kaki_5HT_info, 1)) ') ------------------------'])
-    disp(stats_kaki_info.pair(2).table)
-    disp(['Kaki (NaCl vs 5HT; n=' num2str(size(kaki_5HT_info, 1)+size(kaki_nacl_info, 1)) ') ---------'])
-    disp(stats_kaki_info.table)
-    
-    stats_mango_info = pair_tests(mango_nacl_info, mango_5HT_info);
-    disp(['Mango (NaCl; n=' num2str(size(mango_nacl_info, 1)) ') ------------------------'])
-    disp(stats_mango_info.pair(1).table)
-    disp(['Mango (5HT; n=' num2str(size(mango_5HT_info, 1)) ') ------------------------'])
-    disp(stats_mango_info.pair(2).table)
-    disp(['Mango (NaCl vs 5HT; n=' num2str(size(mango_5HT_info, 1)+size(mango_nacl_info, 1)) ') ---------'])
-    disp(stats_mango_info.table)
-    
-    stats_all_info = pair_tests([kaki_nacl_info; mango_nacl_info], [kaki_5HT_info; mango_5HT_info]);
-    disp(['Both (NaCl; n=' num2str(size(mango_nacl_info, 1)+size(kaki_nacl_info, 1)) ') ------------------------'])
-    disp(stats_all_info.pair(1).table)
-    disp(['Both (5HT; n=' num2str(size(mango_5HT_info, 1)+size(kaki_5HT_info, 1)) ') ------------------------'])
-    disp(stats_all_info.pair(2).table)
-    disp(['Both (NaCl vs 5HT; n=' num2str(size(mango_nacl_info, 1)+size(mango_5HT_info, 1)+size(kaki_nacl_info, 1)+size(kaki_5HT_info, 1)) ') ---------'])
-    disp(stats_all_info.table)
-    
-    
     % FR control
     subplot(lenm, 3, 3+3*(k-1))
     x = met(:, 7+2*(k-1));
@@ -127,7 +60,77 @@ for k = 1:lenm % metric
     ylabel('low FR')
 end
     
-% Collect the data for correlations and information for each monkey
+
+% Collect the data for correlations and information for each monkey and
+% condition
+kaki_nacl_corr = met(met(:,1)==0 & met(:,2)==0, 3:4);
+kaki_5HT_corr = met(met(:,1)==0 & met(:,2)==1, 3:4);
+kaki_nacl_info = met(met(:,1)==0 & met(:,2)==0, 5:6);
+kaki_5HT_info = met(met(:,1)==0 & met(:,2)==1, 5:6);
+
+mango_nacl_corr = met(met(:,1)==1 & met(:,2)==0, 3:4);
+mango_5HT_corr = met(met(:,1)==1 & met(:,2)==1, 3:4);
+mango_nacl_info = met(met(:,1)==1 & met(:,2)==0, 5:6);
+mango_5HT_info = met(met(:,1)==1 & met(:,2)==1, 5:6);
+
+
+% Perform statistical tests for Correlation data for each monkey (and
+% together)
+disp('Stats for Correlation Data')
+stats_kaki_corr = pair_tests(kaki_nacl_corr, kaki_5HT_corr);
+disp(['Kaki (NaCl; n=' num2str(size(kaki_nacl_corr, 1)) ') ------------------------'])
+disp(stats_kaki_corr.pair(1).table)
+disp(['Kaki (5HT; n=' num2str(size(kaki_5HT_corr, 1)) ') ------------------------'])
+disp(stats_kaki_corr.pair(2).table)
+disp(['Kaki (NaCl vs 5HT; n=' num2str(size(kaki_5HT_corr, 1)+size(kaki_nacl_corr, 1)) ') ---------'])
+disp(stats_kaki_corr.table)
+
+stats_mango_corr = pair_tests(mango_nacl_corr, mango_5HT_corr);
+disp(['Mango (NaCl; n=' num2str(size(mango_nacl_corr, 1)) ') ------------------------'])
+disp(stats_mango_corr.pair(1).table)
+disp(['Mango (5HT; n=' num2str(size(mango_5HT_corr, 1)) ') ------------------------'])
+disp(stats_mango_corr.pair(2).table)
+disp(['Mango (NaCl vs 5HT; n=' num2str(size(mango_5HT_corr, 1)+size(mango_nacl_corr, 1)) ') ---------'])
+disp(stats_mango_corr.table)
+
+stats_all_corr = pair_tests([kaki_nacl_corr; mango_nacl_corr], [kaki_5HT_corr; mango_5HT_corr]);
+disp(['Both (NaCl; n=' num2str(size(mango_nacl_corr, 1)+size(kaki_nacl_corr, 1)) ') ------------------------'])
+disp(stats_all_corr.pair(1).table)
+disp(['Both (5HT; n=' num2str(size(mango_5HT_corr, 1)+size(kaki_5HT_corr, 1)) ') ------------------------'])
+disp(stats_all_corr.pair(2).table)
+disp(['Both (NaCl vs 5HT; n=' num2str(size(mango_nacl_corr, 1)+size(mango_5HT_corr, 1)+size(kaki_nacl_corr, 1)+size(kaki_5HT_corr, 1)) ') ---------'])
+disp(stats_all_corr.table)
+
+
+% Perform statistical tests for Information data for each monkey (and
+% together)
+disp("Stats for Mutual Information Data")
+stats_kaki_info = pair_tests(kaki_nacl_info, kaki_5HT_info);
+disp(['Kaki (NaCl; n=' num2str(size(kaki_nacl_info, 1)) ') ------------------------'])
+disp(stats_kaki_info.pair(1).table)
+disp(['Kaki (5HT; n=' num2str(size(kaki_5HT_info, 1)) ') ------------------------'])
+disp(stats_kaki_info.pair(2).table)
+disp(['Kaki (NaCl vs 5HT; n=' num2str(size(kaki_5HT_info, 1)+size(kaki_nacl_info, 1)) ') ---------'])
+disp(stats_kaki_info.table)
+
+stats_mango_info = pair_tests(mango_nacl_info, mango_5HT_info);
+disp(['Mango (NaCl; n=' num2str(size(mango_nacl_info, 1)) ') ------------------------'])
+disp(stats_mango_info.pair(1).table)
+disp(['Mango (5HT; n=' num2str(size(mango_5HT_info, 1)) ') ------------------------'])
+disp(stats_mango_info.pair(2).table)
+disp(['Mango (NaCl vs 5HT; n=' num2str(size(mango_5HT_info, 1)+size(mango_nacl_info, 1)) ') ---------'])
+disp(stats_mango_info.table)
+
+stats_all_info = pair_tests([kaki_nacl_info; mango_nacl_info], [kaki_5HT_info; mango_5HT_info]);
+disp(['Both (NaCl; n=' num2str(size(mango_nacl_info, 1)+size(kaki_nacl_info, 1)) ') ------------------------'])
+disp(stats_all_info.pair(1).table)
+disp(['Both (5HT; n=' num2str(size(mango_5HT_info, 1)+size(kaki_5HT_info, 1)) ') ------------------------'])
+disp(stats_all_info.pair(2).table)
+disp(['Both (NaCl vs 5HT; n=' num2str(size(mango_nacl_info, 1)+size(mango_5HT_info, 1)+size(kaki_nacl_info, 1)+size(kaki_5HT_info, 1)) ') ---------'])
+disp(stats_all_info.table)
+    
+    
+% Collect the FR data for correlations and information for each monkey
 kaki_corr = met(met(:,1)==0, 7:8);
 kaki_info = met(met(:,1)==0, 9:10);
 
